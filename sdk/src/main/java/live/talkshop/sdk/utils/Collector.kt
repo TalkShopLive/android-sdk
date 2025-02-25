71be10ab6f82dbcfe378eb009d9ae20e4604bf98
Ali Kazal <ali.kazal11@gmail.com>
2025-02-25 13:26:21 -0700

    AND-63 Update/Create new definition for the External collector actions AND-65 Products View Collector External Actions AND-66 Show View Collector External Actions AND-67 Remove Internal collector actions

--- sdk/src/main/java/live/talkshop/sdk/utils/Collector.kt
+++ sdk/src/main/java/live/talkshop/sdk/utils/Collector.kt
@@ -1,9 +1,14 @@
 package live.talkshop.sdk.utils
 
+import android.content.res.Resources
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import live.talkshop.sdk.core.authentication.isDNT
+import live.talkshop.sdk.core.show.models.ShowModel
+import live.talkshop.sdk.resources.CollectorActions
+import live.talkshop.sdk.resources.Constants.COLLECTOR_CAT_PAGE_VIEW
+import live.talkshop.sdk.resources.Constants.COLLECTOR_CAT_PROCESS
 import live.talkshop.sdk.resources.URLs.getCollectorUrl
 import live.talkshop.sdk.utils.networking.APIHandler
 import live.talkshop.sdk.utils.networking.HTTPMethod
@@ -11,16 +16,17 @@ import org.json.JSONObject
 
 internal class Collector private constructor() {
     private suspend fun collect(
-        action: String,
+        action: CollectorActions,
         category: String,
         userId: String? = "NOT_SET",
-        eventID: String? = "NOT_SET",
         showKey: String? = "NOT_SET",
         storeId: String? = "NOT_SET",
         videoKey: String? = "NOT_SET",
         showStatus: String? = "NOT_SET",
+        videoTime: String? = "NOT_SET",
     ) {
         val timestamp = System.currentTimeMillis()
+        val display = Resources.getSystem().displayMetrics
         val payload = JSONObject().apply {
             put("timestamp_utc", timestamp)
             put("user_id", userId)
@@ -30,26 +36,21 @@ internal class Collector private constructor() {
             put("application", "android")
             put("meta", JSONObject().apply {
                 put("external", true)
-                put("event_id", eventID)
                 put("streaming_content_key", showKey)
                 put("store_id", storeId)
                 put("video_key", videoKey)
                 put("video_status", showStatus)
+                put("video_time", videoTime)
             })
-            put("utm", JSONObject().apply {
-                put("source", "NOT_SET")
-                put("campaign", "NOT_SET")
-                put("medium", "NOT_SET")
-                put("term", "NOT_SET")
-                put("content", "NOT_SET")
+            put("aspect", JSONObject().apply {
+                put("browser_resolution", "NOT_SET")
+                put("screen_resolution", "${display.heightPixels} x ${display.widthPixels}")
             })
         }
 
         try {
             APIHandler.makeRequest(
-                requestUrl = getCollectorUrl(),
-                requestMethod = HTTPMethod.POST,
-                payload = payload
+                requestUrl = getCollectorUrl(), requestMethod = HTTPMethod.POST, payload = payload
             )
         } catch (e: Exception) {
             Logging.print(Collector::class.java, e)
@@ -76,28 +77,23 @@ internal class Collector private constructor() {
         }
 
         fun collect(
-            action: String,
-            category: String,
+            action: CollectorActions,
+            show: ShowModel? = null,
             userId: String? = null,
-            eventID: String? = null,
-            showKey: String? = null,
-            storeId: String? = null,
-            videoKey: String? = null,
-            showStatus: String? = null,
         ) {
             if (isDNT) {
                 return
             }
             CoroutineScope(Dispatchers.IO).launch {
                 getInstance().collect(
-                    action,
-                    category,
-                    userId,
-                    eventID,
-                    showKey,
-                    storeId,
-                    videoKey,
-                    showStatus,
+                    action = action,
+                    category = if (action == CollectorActions.VIEW_CONTENT) COLLECTOR_CAT_PAGE_VIEW else COLLECTOR_CAT_PROCESS,
+                    userId = userId,
+                    showKey = show?.showKey,
+                    storeId = show?.id?.toString(),
+                    videoKey = show?.eventId,
+                    showStatus = show?.status,
+                    videoTime = show?.duration?.toString()
                 )
             }
         }
